924a45cea67dce602a852337155f9f8c9da765ed
musa al qadri <musaqazim1@gmail.com>
2023-10-21 23:54:41 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,118 @@
+// document.write ("<h2>sum of 3 and 5 is  <h2/>")
+// document.write +(num1 = 3 )
+// document.write +(num2 = 5)
+// var num3 = num1 + num2
+
+// document.write(num3)
+
+// document.write ("<h2>sum of 3 and 5 is  <h2/>")
+// document.write +(num1 = 3 )
+// document.write +(num2 = 5)
+// var num3 = num1 - num2
+
+// document.write(num3)
+
+// document.write ("<h2>sum of 3 and 5 is  <h2/>")
+// document.write +(num1 = 3 )
+// document.write +(num2 = 5)
+// var num3 = num1 * num2
+
+// document.write(num3)
+
+// document.write ("<h2>sum of 3 and 5 is  <h2/>")
+// document.write +(num1 = 3 )
+// document.write +(num2 = 5)
+// var num3 = num1 / num2
+
+// document.write(num3)
+
+let a = 7;
+let b = 2;
+let c = 5;
+let sum = a+b;
+let sun = a+c;
+let sub = a-b;
+let multiply = a*b;
+let divide = a/b;
+let modulus = a%b;
+document.write('<h1>Question no 1:~<h1/>')
+document.write(`<h2>Sum of ${a}and ${b} is: ${sum}<h2/>`)
+document.write(`<br><h1>Question no 2:~<h1/>`)
+document.write(`<h2>Sub of ${a} and ${b} is: ${sub}</h2>`)
+document.write(`<h2>Multiply of ${a} and ${b} is: ${multiply}</h2>`)
+document.write(`<h2>Divide of ${a} and ${b} is: ${divide}</h2>`)
+document.write(`<h2>Modulus of ${a} and ${b} is: ${modulus}</h2>`)
+document.write(`<br><h1>Question no 3:~<h1/>`)
+document.write(`<h2>Value after variable declararion is: Undefined<h2/>`)
+document.write(`<h2>Initial value is: ${c}<h2/>`)
+document.write(`<h2>value after increnment is: ${++c}<h2/>`)
+document.write(`<h2>value after addition is: ${c+a}<h2/>`)
+document.write(`<h2>value after decrenment is: ${--a+c}<h2/>`)
+document.write(`<h2>The reminder is: ${12%3}<h2/>`)
+let ticket = 600;
+document.write(`<br><h1>Question no 4:~<h1/>`)
+document.write(`<h2>Total cost to buy 5 tickets to a movie is ${ticket*5}PKR<h2/>`)
+document.write(`<br><h1>Question no 5:~<h1/>`)
+document.write(`<h2>Table of 4<h2/>`)
+for (var i=1;i<11;i++){
+    var tablenum = 4;
+document.write(`<h4>${tablenum} X ${i} = ${tablenum*i}<h4/>`)    
+}
+document.write(`<br><h1>Question no 6:~<h1/>`)
+let celsius=97;
+let f=(celsius*(9/5))+32;
+document.write(`<h3>${celsius}°C is ${f}°F<h3/>`)
+let forn=30
+let d=(forn-32)*5/9
+document.write(`<h3>${forn}°f is ${d}<h3/>`)
+document.write(`<br><h1>Question no 7:~<h1/>`)
+document.write(`<h2>Shopping Cart<h2/>`)
+let item1=650;
+let item2=100;
+let shipping=100;
+document.write(`<h4>Price of item 1 is ${item1} <br>
+Quantity of item 1 is 3 <br>
+Price of item 2 is ${item2} <br>
+Quantity of item 2 is 7 <br>
+ Shipping Charges ${shipping} <h4/>`)
+ document.write(`<h3>Total cost of your order is ${item1*3+item2*7+shipping} <h3/>`)
+document.write(`<br><h1>Question no 8:~<h1/>`)
+document.write(`<h2>Mark Sheet<h2/>`)
+var totalMark = 980;
+var markObtain = 804;
+var percentge = (markObtain/totalMark)*100
+var per = ('percentage:')
+var percentag = per +" "+percentge
+document.write(`<h4>Total Marks:980<h4/>`)
+document.write(`<h4>Marks Obtain:840<h4/>`)
+document.write(percentag );
+document.write(`<br><h1>Question no 9:~<h1/>`)
+document.write(`<h2>Currency in PKR</h2>`)
+document.write(`<h4>Total currency in PKR : ${(10*104.80)+(25*28)} <h4/>`)
+document.write(`<br><h1>Question no 10:~<h1/>`)
+document.write(`<h4>${a+5*10/2} <h4/>`)
+document.write(`<br><h1>Question no 11:~<h1/>`)
+let currentYear = 2023
+let birthYear = 2001
+document.write(`<h4>Current year: 2023 <br>
+Birth year : 2001<br>
+your age is : ${currentYear-birthYear}<h4/>`)
+document.write(`<br><h1>Question no 12:~<h1/>`)
+let radius =20
+document.write(`<h4>Radius of a Circle is : ${radius}<br>
+The circumfrance is:${2*3.142*20}<br>
+The area is:${3.142*(radius**2)} <h4/>`)
+document.write(`<br><h1>Question no 13:~<h1/>`)
+document.write(`<h2>Life line supply calculator<h2/>`)
+document.write(`<br>`)
+
+document.write(`<br><h1>Question no 1:~<h1/>`)
+document.write(`<h2>Result<h2/>`)
+document.write
+
+
+
+
+
+
+
